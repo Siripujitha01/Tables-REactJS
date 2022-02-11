54c8785b1a3995d720ed3aec72bbcd7409b0d0a7
pujitha <siripujitha9701@gamil.com>
2022-02-11 22:37:52 +0530

    REACT-JS

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Comment from "./comment.js";
+const App=()=>{
+  return(
+    <Comment guess={[22,15,12,11]}/>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
